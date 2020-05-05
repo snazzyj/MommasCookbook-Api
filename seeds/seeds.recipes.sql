5d90c7c461c441c0cbb9f5ad9a332dcd0c4ec051
AJ Olson <silentx.alex@gmail.com>
2020-05-05 14:42:57 -0400

    added mock data to seed database

---
+++ seeds/seeds.recipes.sql
@@ -0,0 +1,33 @@
+INSERT INTO recipes (creator_id, recipe_name, ingredients, 
+                    directions, prepTime, cookTime, servingSize)
+VALUES
+    (
+        1, 'Stuffed Red Pepper',  '{"2 Chicken breasts diced", "1 onion, diced", "1/4 cup of salt and pepper", "1 green pepper"}',
+         '{"Preheat oven to 350 degrees", "Add onions and green peppers into red pepper", "Put red peppers into oven for 30 minutes"}',
+        '15 Minutes', '30 Minutes', '1-2'
+    ),
+    (
+        2, 'Stuffed Blue Pepper',  '{"2 Chicken breasts diced", "1 onion, diced", "1/4 cup of salt and pepper", "1 green pepper"}',
+         '{"Preheat oven to 350 degrees", "Add onions and green peppers into red pepper", "Put red peppers into oven for 30 minutes"}',
+        '15 Minutes', '30 Minutes', '1-2'
+    ),
+    (
+        2, 'Stuffed Green Pepper',  '{"2 Chicken breasts diced", "1 onion, diced", "1/4 cup of salt and pepper", "1 green pepper"}',
+         '{"Preheat oven to 350 degrees", "Add onions and green peppers into red pepper", "Put red peppers into oven for 30 minutes"}',
+        '15 Minutes', '30 Minutes', '1-2'
+    ),
+    (
+        3, 'Stuffed Yellow Pepper',  '{"2 Chicken breasts diced", "1 onion, diced", "1/4 cup of salt and pepper", "1 green pepper"}',
+         '{"Preheat oven to 350 degrees", "Add onions and green peppers into red pepper", "Put red peppers into oven for 30 minutes"}',
+        '15 Minutes', '30 Minutes', '1-2'
+    ),
+    (
+        3, 'Stuffed Purple Pepper',  '{"2 Chicken breasts diced", "1 onion, diced", "1/4 cup of salt and pepper", "1 green pepper"}',
+         '{"Preheat oven to 350 degrees", "Add onions and green peppers into red pepper", "Put red peppers into oven for 30 minutes"}',
+        '15 Minutes', '30 Minutes', '1-2'
+    ),
+    (
+        1, 'Stuffed Orange Pepper',  '{"2 Chicken breasts diced", "1 onion, diced", "1/4 cup of salt and pepper", "1 green pepper"}',
+         '{"Preheat oven to 350 degrees", "Add onions and green peppers into red pepper", "Put red peppers into oven for 30 minutes"}',
+        '15 Minutes', '30 Minutes', '1-2'
+    )
---
+++ seeds/seeds.users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (first_name, last_name, email, password)
+VALUES
+    ('Alex', 'Olson', 'silentx.alex@gmail.com', 'Password123'),
+    ('Leyna', 'Fleckenstein', 'imaqt314@gmail.com', 'Password123'),
+    ('Charleigh', 'Grace', 'fiestyhoe@gmail.com', 'Password123')
