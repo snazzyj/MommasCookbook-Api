f4d4a0f099a4fb8b3534ea578321542e9ab93955
AJ Olson <silentx.alex@gmail.com>
2020-05-07 15:13:09 -0400

    added auth and recipes routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const {NODE_ENV} = require('./config');
+const userAuth = require('./auth/users-auth');
+const recipeRoute = require('./recipes/recipe-route');
 
 const app = express();
 
@@ -14,6 +16,14 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+// route for login and sign up. creates jwt token
+// gets recipe data for user upon login
+app.use('/api/auth', userAuth);
+
+//route to post a new recipe, inserts recipe into db
+//get req to get a recipe by id
+app.use('/api/recipes', recipeRoute);
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
