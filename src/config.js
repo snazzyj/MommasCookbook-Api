bd755ba0de1dfe06ab2cd6c86733e4dbe9bc4c88
AJ Olson <silentx.alex@gmail.com>
2020-05-07 15:13:56 -0400

    added jwt token

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://alex:1@localhost/cookbook-api'
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://alex:1@localhost/cookbook-api',
+    JWT_SECRET: process.env.JWT_SECRET || 'cookbook-special-secret'
   }
