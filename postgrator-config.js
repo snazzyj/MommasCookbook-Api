5f5567e85d9cae009f0aa9c9e4d5ef80a828ee26
AJ Olson <silentx.alex@gmail.com>
2020-08-19 14:53:45 -0400

    added .env file. updated config

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,6 +6,7 @@ module.exports = {
     "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
+    "ssl": !!process.env.SSL,
     "database" : "cookbook-api",
     "username" : "alex",
     "password": "1"
